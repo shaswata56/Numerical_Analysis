891b43c6b243e37b911bbb82b31b2a897e6c70a0
shaswata56 <shaswata56@gmail.com>
2019-10-29 10:46:50 +0600

    demo euler method added

--- Assignment/RootFindingMethods/euler_method.m
+++ Assignment/RootFindingMethods/euler_method.m
@@ -1,10 +1,17 @@
-h = 0.1;
-t = 0:h:2;
-ystar = zeros(size(t));
-ystar(1) = 9;
-f = @(j, k) -2 * j + cos(4 * k);
-for i=1:(length(t)-1)
-    k =  f(ystar(i), t(i));
-    ystar(i + 1) = ystar(i) + k * h;
+function euler_method()
+    h = 0.1
+    fprintf("f(x) = -2 * ystar(x) + cos(4 * t(x))\n");
+    f = @(j, l) -2 * j + cos(4 * l);
+    ret = EulersMethod(h, f)
 end
-Table = [t', ystar']
+
+function ret = EulersMethod(h, f)
+    t = 0:h:1;
+    ystar = zeros(size(t));
+    ystar(1) = 0;
+    for i=1:(length(t)-1)
+        k =  f(ystar(i), t(i));
+        ystar(i + 1) = ystar(i) + k * h;
+    end
+    ret = [t', ystar'];
+end
